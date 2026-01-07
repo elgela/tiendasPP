52036f09825edf78bd8b37ab33cd65c749ee4cdc
Marcelo Gelato <gelatomarcelo@hotmail.com>
2026-01-07 14:24:28 -0300

    serviceWorker

--- js/main.js
+++ js/main.js
@@ -8,3 +8,31 @@ menuBtn.addEventListener('click', () => {
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.toggle('active');
 });
+
+///////service worker/////////
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/sw.js")
+    .then(() => console.log("Service Worker registrado"));
+}
+
+let deferredPrompt;
+
+window.addEventListener("beforeinstallprompt", (e) => {
+  e.preventDefault();
+  deferredPrompt = e;
+
+  const btn = document.getElementById("installBtn");
+  btn.style.display = "block";
+
+  btn.addEventListener("click", () => {
+    deferredPrompt.prompt();
+    deferredPrompt.userChoice.then(choice => {
+      if (choice.outcome === "accepted") {
+        console.log("App instalada");
+      } else {
+        console.log("Instalación cancelada");
+      }
+      deferredPrompt = null;
+    });
+  });
+});
